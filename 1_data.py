6a2f55e945b90c84790f9872ea41941c0338e226
wicak <ansufw@gmail.com>
2021-03-08 14:35:38 +0100

    stater pack python

---
+++ 1_data.py
@@ -0,0 +1,47 @@
+#####################
+# data type in Python #
+#####################
+# 1. Give example fundamental: int, bool, float, str
+# 2. Give example collection: list, dict, set, tupple
+# 3. how to check data type: type()
+# 4. how to convert data type: str(), int()
+
+
+# integer
+# age = 37
+# print(age)
+# print(type(age))
+
+# bool
+# is_married = True
+# print(is_married)
+# print(type(is_married))
+
+# float
+# GPA = 3.9
+# print(type(GPA))
+
+# string
+# nama = "John"
+# print(type(nama))
+
+# list 
+nilai = [3,4,5,3,5,4,3]
+print(type(nilai))
+
+# set
+# sample = {3,4,5,4,3,4,1,4}
+# print(sample)
+# print(type(sample))
+
+# tupple
+# tup = (2,3,4,5,4)
+# print(type(tup))
+
+# dictionary
+# dict = {
+#     "nama":"Jack",
+#     "usia":50
+# }
+# print(type(dict))
+
